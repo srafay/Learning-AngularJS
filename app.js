95e8695367681fbd0449b29ce442071ea2d91343
Syed Rafay <rafay_syed1@yahoo.com>
2017-05-13 21:30:23 +0500

    Playing with more of the 'ng' directives

--- app.js
+++ app.js
@@ -1,6 +1,19 @@
 var appname = angular.module('store', []);
-appname.controller('greetings', ['$scope',
-function($scope){
-    $scope.vars = {hi:"Hello World!",
-question:"How you doin?"};
+appname.controller('Product',['$scope',
+    function($scope){
+        $scope.products = [
+            {name : "Diamond1",
+            price : 45,
+            description : "Pure white diamond",
+            canPurchase : true},
+            {name : "Diamond2",
+            price : 50,
+            description : "Pure black diamond",
+            canPurchase : true},
+            {name : "Diamond3",
+            price : 100000,
+            description : "Nihilistic diamond",
+            canPurchase : 0}
+            ];
 }]);
+console.log("hahaha");
