ed8bbf52a264e2b5293594d8941ea75eb2c7ff8e
Syed Rafay <rafay_syed1@yahoo.com>
2017-05-15 02:31:30 +0500

    Trying angular tabs with expressions

--- app.js
+++ app.js
@@ -1,19 +1,34 @@
 var appname = angular.module('store', []);
-appname.controller('Product',['$scope',
+appname.controller('items',['$scope',
     function($scope){
         $scope.products = [
             {name : "Diamond1",
             price : 45,
             description : "Pure white diamond",
             canPurchase : true},
+
             {name : "Diamond2",
             price : 50,
             description : "Pure black diamond",
             canPurchase : true},
+
             {name : "Diamond3",
             price : 100000,
             description : "Nihilistic diamond",
-            canPurchase : 0}
+            canPurchase : false},
             ];
 }]);
+
+appname.controller('panelSelecter', function(){
+        this.tab = 1;
+
+        this.selectTab = function(newTab){
+            this.tab=newTab;
+        };
+
+        this.isSelected = function(Tab){
+            return Tab === this.tab;
+        };
+
+});
 console.log("hahaha");
