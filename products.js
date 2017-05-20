f14ec0d1272851eab6d462eb413d3cb234756d0d
Syed Rafay <rafay_syed1@yahoo.com>
2017-05-20 22:20:51 +0500

    Injecting  service for fetching the data for products from a json file

--- products.js
+++ products.js
@@ -2,47 +2,14 @@
 
     var appname = angular.module('store-products', []);
 
-    appname.controller('items',function(){
-            this.products = [
-                {name : "Diamond1",
-                price : 45,
-                description : "Pure white diamond",
-                canPurchase : true,
-                reviews: [
-                    {
-                    name:"Riki",
-                    email:"riki@dota2.com",
-                    details:"Nice product",
-                    stars:5}
-                ]
-                },
-
-                {name : "Diamond2",
-                price : 50,
-                description : "Pure black diamond",
-                canPurchase : true,
-                reviews: [
-                    {
-                    name:"Alchemist",
-                    email:"Alc@dota2.com",
-                    details:"Diamond2 was shiny",
-                    stars:4}
-                ]
-                },
-
-                {name : "Diamond3",
-                price : 100000,
-                description : "Nihilistic diamond",
-                canPurchase : false,
-                reviews: [
-                    {
-                    name:"Anti-Mage",
-                    email:"am@dota2.com",
-                    details:"Need more blinking product",
-                    stars:2}
-                ]
-                },
-                ];
-    });
+    appname.controller('items', ['$http', function($http){
+        this.products = [];
+        var store = this;
+        $http.get('store-products.json').success(function(data){
+            store.products = data;
+            console.log("loading produts.json");
+            console.log(data);
+        });
+    }]);
 
 })();
